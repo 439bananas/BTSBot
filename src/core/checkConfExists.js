4820d4af2454946a114e568375ce22657eaea413
Banana <banana@439bananas.local>
2021-07-27 19:43:53 +0100

    interim

--- src/core/checkConfExists.js
+++ src/core/checkConfExists.js
@@ -12,10 +12,20 @@
 
 const fs = require('fs');
 const path = require('path');
+const checkmysql = require('./checkMySQL')
 
 async function checkforconf() {
     if (fs.existsSync(path.join(__dirname, '..', 'configs', 'conf.json'))) { // Does src/configs/conf.json exist? If yes, return true. Else, false
-        return true;
+        const conf = require('../configs/conf.json')
+        checkmysql(conf.hostname, conf.username, conf.password, conf.db).then(result => {
+            if (conf.hostname == undefined || conf.username == undefined || conf.password == undefined || conf.db == undefined) {
+                return 'MISSING_FIELDS';
+            }
+            if (result == 'OK') {
+                return true;
+            }
+        }).catch(err => {return err;})
+
     }
     else {
         return false;
