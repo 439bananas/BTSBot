d2bfeb256683fefdce5448cc28f2a977fe887759
439bananas <me@439bananas.com>
2023-01-01 10:34:33 +0000

    forgot to remove assertions from getApplicationId

--- src/core/getApplicationId.js
+++ src/core/getApplicationId.js
@@ -18,7 +18,6 @@ function getid(token) {
     return new Promise(function (resolve, reject) {
         if (tokencache != token) { // Let's try and minimise our requests to Discord as much as possible
             tokencache = token
-            log.temp("fetch for getApplicationId.js")
             fetch('https://discord.com/api/v10/oauth2/applications/@me', { // Fetch OAuth2 endpoint
                 method: 'GET',
                 headers: {
@@ -41,7 +40,6 @@ function getid(token) {
                     }
                 }).catch(err => {
                     if (err.name == "FetchError") {
-                        log.temp("getApplicationId failed")
                         reject("CANNOT_CONNECT_TO_DISCORD")
                     }
                 })
