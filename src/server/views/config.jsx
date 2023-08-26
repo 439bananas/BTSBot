3884a189997f7ca3f2cb078e34ecf2045fa3df13
439bananas <me@439bananas.com>
2023-08-26 22:03:26 +0100

    i think that's config (the long and boring part) complete. let's move on

--- src/server/views/config.jsx
+++ src/server/views/config.jsx
@@ -18,23 +18,6 @@ const ErrorPage = require('./error-page-spa');
 const CreateConfigPassword = require('./create-configuration-password');
 const RequestConfigPassword = require('./enter-configuration-password');
 const ConfigForm = require('./config-form');
-// small problem for config
-// how on earth are we supposed to deal with oauth2 at this point??
-// call an api which checks if oauth2 validation is required
-// if response is "OAUTH_OK" then show the user the "config complete" page. THE API ENDPOINT SHOULD AUTOMATICALLY RESTART THE APPLICATION
-// if it's "OAUTH_REDIRECT" then redirect to the specified url
-// if it's "OAUTH_FAIL" then show a failure
-// if "TOKEN_FAIL" then show that error
-// if "NO_CONF" then just show the page without any failures
-// if "CONF_OK" then display a 404
-// if "REFRESH_PAGE", redirect to /config without the code
-// if "BAD_CLIENT_SECRET", show the same page but show the bad client secret error
-// if "CANNOT_CONNECT_TO_MICROSOFT", show the wall and show that error
-// if "CANNOT_CONNECT_TO_DISCORD", ditto as for microsoft (but show the discord error)
-// if "CANNOT_CONNECT_TO_GOOGLE", once again, the same thing again for google
-// else, show the wall and the error
-// if the conf does exist, get the config from the api and set these options as default
-// the api will need to be modified to get the entire config, excluding any and all passwords or tokens
 
 function getOauthStatus(query) { // See if OAuth is ok, and if not, return any errors
     const [oauthStatus, setOauthStatus] = useState([])
@@ -137,56 +120,20 @@ function Config(props) {
                 break;
         }
     } else {
-        if (oauthStatus.error == "OAUTH_FAIL" || oauthStatus.error == "NO_CONF") {
+        if (oauthStatus.error == "OAUTH_FAIL" || oauthStatus.error == "NO_CONF") { // Validate authentication if no conf or OAuth fail, else show wall (or if conf exists, show 404)
             returnedValue = <CheckAuth languages={languages.languages} token={props.cookies.configtoken} error={oauthStatus.error} configPassword={configPassword} language={props.language} uniconf={props.uniconf} settings={settings} />
         } else if (oauthStatus.error == "CONF_OK") {
             returnedValue = <Error404 language={props.language} confErr={props.confErr} uniconf={props.uniconf} />
+        } else if (oauthStatus.error == "CANNOT_CONNECT_TO_MICROSOFT") {
+            returnedValue = <ErrorPage error={uniconf.projname + translate(props.language, 'page_wallcannotconnecttomicrosoft')} diag={translate(props.language, 'page_wallcannotconnecttomicrosoftdiagpart1') + uniconf.projname + translate(props.language, 'page_wallcannotconnecttomicrosoftdiagpart2')} />
+        } else if (oauthStatus.error == "CANNOT_CONNECT_TO_DISCORD") {
+            returnedValue = <ErrorPage error={uniconf.projname + translate(props.language, 'page_wallcannotconnecttodiscord')} diag={translate(props.language, 'page_wallcannotconnecttodiscorddiagpart1') + uniconf.projname + translate(props.language, 'page_wallcannotconnecttodiscorddiagpart2')} />
+        } else if (oauthStatus.error == "CANNOT_CONNECT_TO_GOOGLE") {
+            returnedValue = <ErrorPage error={uniconf.projname + translate(props.language, 'page_wallcannotconnecttogoogle')} diag={translate(props.language, 'page_wallcannotconnecttogooglediagpart1') + uniconf.projname + translate(props.language, 'page_wallcannotconnecttogooglediagpart2')} />
+        } else {
+            returnedValue = <ErrorPage error={uniconf.projname + translate(props.language, 'page_configoauth2unknownerror')} diag={JSON.stringify(oauthStatus)} />
         }
-        // cannot connect to x? show wall with that error
-        // else show error on wall
-
-        /*
-        switch (oauthStatus.error) {
-            case "OAUTH_FAIL":
-                returnedValue = null
-                // new api endpoint
-                // this might serve a similar purpose to confPage.js???
-                // default-conf-settings
-                // if conf ok then return CONF_OK
-                // else see what the error is
-                // if the error is MISSING_FIELDS then call a function that determines what settings are missing, etc
-                // if the error is false then call that same function after checking interim conf to see if the conf exists
-                // else, return unknown error
-                // oauth has failed, show bad client secret error
-                // hold up because we also need to deal with authorisation???
-                break;
-            case "NO_CONF":
-                returnedValue = null
-                // show normal conf
-                break;
-            case "CONF_OK":
-                returnedValue = null
-                // show 404 error
-                break;
-            case "CANNOT_CONNECT_TO_MICROSOFT":
-                returnedValue = null
-                // show wall with cannot connect to ms
-                break;
-            case "CANNOT_CONNECT_TO_DISCORD":
-                returnedValue = null
-                // show wall with cannot connect to discord
-                break;
-            case "CANNOT_CONNECT_TO_GOOGLE":
-                returnedValue = null
-                // show wall with cannot connect to google
-                break;
-            default:
-                // show wall plus the respective error
-                returnedValue = null
-                break;
-        }*/
     }
-    //return(JSON.stringify(getOauthStatus()))
 
     if (!returnedValue) returnedValue = null;
 
