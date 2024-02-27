4780c232fd646a1b564116af64fd8c865b42ecf5
439bananas <me@439bananas.com>
2024-02-27 17:26:02 +0000

    more performane improvements

--- src/core/getLanguageJSON.js
+++ src/core/getLanguageJSON.js
@@ -10,13 +10,34 @@
 //                                                         //
 /////////////////////////////////////////////////////////////
 
-import { existsSync } from 'fs'
-import { join } from 'path'
-import { dirname } from 'path';
+import { existsSync, readdirSync } from 'fs'
+import { dirname, basename, join } from 'path'
 import { fileURLToPath, pathToFileURL } from 'url';
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 let conf
+let language
+
+let languages = {}
+let langsDir
+if (basename(process.argv[1]) === "index.js") {
+    langsDir = join(__dirname, "..", "i18n")
+} else {
+    langsDir = join(__dirname, "..", "src", "i18n")
+}
+
+let langsDirListing = readdirSync(langsDir) // Put each of the language files into an object at the start of runtime, improve performance
+
+langsDirListing.map(file => {
+    let fileName = file.split('.')
+    if (fileName[fileName.length - 1] == "json") {
+        import(pathToFileURL(join(langsDir, file)), { assert: { type: "json" } }).then(mod => {
+            if (mod.default.name) {
+                languages[file.split(".")[0]] = mod.default
+            }
+        })
+    }
+})
 
 function getlang(noext, userid) { // Gets the configured language (noext is an argument that lets the function know whether to send the file extension too)
     let ext
@@ -26,15 +47,8 @@ function getlang(noext, userid) { // Gets the configured language (noext is an a
 
     async function getFile(resolve, reject, conf) {
         if (conf.language !== undefined) {
-            if (typeof (pkg) != "undefined" && existsSync(join(__dirname, '..', 'src', 'i18n', conf.language + '.json'))) { // Check if the language in conf.json exists as a json fine
-                let lang = (await import(pathToFileURL('src/i18n/' + conf.language + '.json'), { assert: { type: "json" } })).default
-                if (lang.name !== undefined) { // Check if the language name exists
-                    resolve(conf.language + ext) // If all are true, send back the language JSON file name
-                } else {
-                    resolve(uniconf.defaultlanguage + ext) // Else, return the default language
-                }
-            } else if (typeof (pkg) == "undefined" && existsSync(join(__dirname, '..', 'i18n', conf.language + '.json'))) { // Check if the language in conf.json exists as a json fine
-                let lang = (await import(pathToFileURL(__dirname + '/../i18n/' + conf.language + '.json'), { assert: { type: "json" } })).default
+            if (languages[conf.language]) { // Check if the language in conf.json exists as a json fine
+                let lang = (languages[conf.language])
                 if (lang.name !== undefined) { // Check if the language name exists
                     resolve(conf.language + ext) // If all are true, send back the language JSON file name
                 } else {
@@ -51,8 +65,7 @@ function getlang(noext, userid) { // Gets the configured language (noext is an a
     return new Promise(async function (resolve, reject) {
         if (userid == undefined) {
             if (typeof (pkg) != "undefined") { // pkg is global in server.mjs but not index.js (different process) so checking if it exists means that it's possible to see differentiate what process this is
-                if (existsSync(join(__dirname, '..', 'configs', 'conf.json'))) { // conf.json is preferred, so follow the process if conf.json exists:
-                    conf = (await import("../../configs/conf.json", { assert: { type: "json" } })).default
+                if (typeof(conf) != "undefined") { // conf.json is preferred, so follow the process if conf.json exists:
                     getFile(resolve, reject, conf)
                 } else if (existsSync(join(__dirname, '..', 'configs', 'confinterim.json'))) { // Ditto but check if the MySQL configuration (limbo) has the setting
                     conf = (await import('../../configs/confinterim.json', { assert: { type: "json" } })).default
--- src/server/views/components/getLanguageJSON.js
+++ src/server/views/components/getLanguageJSON.js
@@ -10,17 +10,36 @@
 //                                                         //
 /////////////////////////////////////////////////////////////
 
-import { existsSync } from 'fs';
-import { join } from 'path';
-import { dirname } from 'path';
-import { fileURLToPath } from 'url';
+import { existsSync, readdirSync } from 'fs';
+import { basename, dirname, join } from 'path';
+import { fileURLToPath, pathToFileURL } from 'url';
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
-let langfile
+
+let languages = {}
+let langsDir
+if (basename(process.argv[1]) === "index.js") {
+    langsDir = join(__dirname, "..", "i18n")
+} else {
+    langsDir = join(__dirname, "..", "src", "i18n")
+}
+
+let langsDirListing = readdirSync(langsDir) // Put each of the language files into an object at the start of runtime, improve performance
+
+langsDirListing.map(file => {
+    let fileName = file.split('.')
+    if (fileName[fileName.length - 1] == "json") {
+        import(pathToFileURL(join(langsDir, file)), { assert: { type: "json" } }).then(mod => {
+            if (mod.default.name) {
+                languages[file.split(".")[0]] = mod.default
+            }
+        })
+    }
+})
 
 async function getLangFile(languageCode) { // This function gets the contents of the supplied language code, and all of its phrases
-    if (existsSync(join(__dirname, '..', 'src', 'i18n', languageCode + '.json'))) {
-        const langfile = (await import('../src/i18n/' + languageCode + '.json', { assert: { type: "json" } })).default
+    if (languages[languageCode]) {
+        const langfile = languages[languageCode]
         return langfile
     } else {
         return {}
